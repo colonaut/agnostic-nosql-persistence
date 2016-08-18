ca1b6bf708c6527be79b5a143db07a7717756ad4
kalle <linsenboll@cleverbridge.com>
2016-08-18 18:27:36 +0200

    started to rework QueryAnalyzer (which should be renamed to Query)

--- test/query_analyzer.js
+++ test/query_analyzer.js
@@ -9,7 +9,7 @@ const Joi = require('joi');
 
 describe('When creating a Query instance', function() {
 
-    describe('with exact or left approximation string values', function () {
+    describe('with valid string values', function () {
 
         const schema_analyzer = analyze(Joi.object().keys({
             foo: Joi.string(),
@@ -21,25 +21,25 @@ describe('When creating a Query instance', function() {
             foo: 'foo',
             bar: 'ba*',
             buzz: 'bu**'
+            //TODO: right, inner;
         }, schema_analyzer);
 
         it('should the query instance provide a correct exact search object', function () {
+            expect(query.isArray('foo')).to.be.false;
+            expect(query.isNumber('foo')).to.be.false;
             expect(query.value('foo')).to.equal('foo');
-            expect(query.array('foo')).to.equal(undefined);
-            expect(query.left('foo')).to.equal(undefined);
+            expect(query.expr('foo')).to.be.undefined;
+            expect(query.value('foo')).to.be.a.string;
 
-            expect(query.value('bar')).to.equal('ba*');
-            expect(query.array('bar')).to.equal(undefined);
-            expect(query.left('bar')).to.equal('ba');
+            expect(query.value('bar')).to.be.undefined;
+            expect(query.expr('bar')).to.equal('ba*');
 
-            expect(query.value('buzz')).to.equal('bu**');
-            expect(query.array('buzz')).to.equal(undefined);
-            expect(query.left('buzz')).to.equal(undefined);
+            //expect(query.value('buzz')).to.equal('bu*'); //TODO: expr. escaping
+            //expect(query.expr('buzz')).to.be.undefined; //TODO: expr. escaping
         });
     });
 
-    describe('with exact or left approximation array values', function () {
-
+    describe('with valid array values', function () {
         const schema_analyzer = analyze(Joi.object().keys({
             array_foo: Joi.array(),
             array_bar: Joi.array(),
@@ -47,30 +47,37 @@ describe('When creating a Query instance', function() {
         }));
 
         const query = new Query({
-            array_foo: ['foo1', 'foo2', 'foo3'],
-            array_bar: 'ba*',
-            array_buzz: 'bu**'
+            array_foo: ['foo1', 'foo2', 'foo3']
         }, schema_analyzer);
 
-        let exception;
-
         it('should the query instance provide a correct exact search object', function () {
+            expect(query.value('array_foo').length).to.be.an.array;
             expect(query.value('array_foo').length).to.equal(3);
-            expect(query.array('array_foo').length).to.equal(3);
-            expect(query.left('array_foo')).to.equal(undefined);
-
-            expect(query.value('array_bar')).to.equal('ba*');
-            expect(query.array('array_bar')).to.equal(undefined);
-            expect(query.left('array_bar')).to.equal('ba');
-
-            expect(query.value('array_buzz')).to.equal('bu**');
-            expect(query.array('array_buzz')).to.equal(undefined);
-            expect(query.left('array_buzz')).to.equal(undefined);
         });
     });
 
-    describe('with exact number values and left (smaller or equals) values', function () {
+    describe('with string values for an array property', function () {
+        const schema_analyzer = analyze(Joi.object().keys({
+            array_foo: Joi.array(),
+            array_bar: Joi.array(),
+            array_buzz: Joi.array()
+        }));
 
+        try {
+            new Query({
+                array_foo: ['foo1', 'foo2', 'foo3'],
+                array_bar: 'ba*',
+                array_buzz: 1.0000
+            }, schema_analyzer);
+        } catch (exception) {
+            it('should an exception be thrown', function () {
+                //console.log(exception);
+                expect(exception).to.be.an.error;
+            });
+        }
+    });
+
+    describe('with valid umber values', function () {;
         const schema_analyzer = analyze(Joi.object().keys({
             number_foo: Joi.number(),
             number_bar: Joi.number().precision(0),
@@ -79,24 +86,20 @@ describe('When creating a Query instance', function() {
 
         const query = new Query({
             number_foo: 1,
-            number_bar: '1.003*',
-            number_buzz: '1*'
+            number_bar: ' < 1.003',
+            number_buzz: '>=1'
         }, schema_analyzer);
 
-        let exception;
-
-        it('should the query instance provide a correct exact search object', function () {
+        it('should value and expr return the correct values', function () {
             expect(query.value('number_foo')).to.equal(1);
-            expect(query.left('number_foo')).to.equal(undefined);
-            expect(query.number('number_foo')).to.equal(1);
+            expect(query.expr('number_foo')).to.be.undefined;
 
-            expect(query.value('number_bar')).to.equal('1.003*');
-            expect(query.left('number_bar')).to.equal(1.003);
-            expect(query.number('number_bar')).to.equal(1.003);
+            expect(query.expr('number_bar')).to.equal('< 1.003');
+            expect(query.value('number_bar')).to.be.undefined;
 
-            expect(query.value('number_buzz')).to.equal('1*');
-            expect(query.left('number_buzz')).to.equal(1);
-            expect(query.number('number_buzz')).to.equal(1);
+            expect(query.expr('number_buzz')).to.equal('>=1');
+            expect(query.value('number_buzz')).to.be.undefined;
+
         });
     });
 
